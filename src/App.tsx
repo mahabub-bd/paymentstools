f50b5923af35592a43f96cff986bf848291dfa4e
mahabub-bd <palashmahabub>
2026-06-07 22:55:10 +0600

    added track2 genarate

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Dashboard from './components/Dashboard'
 import HomePage from './components/HomePage'
+import { Analytics } from 'node_modules/@vercel/analytics/dist/nuxt/runtime'
 
 function App() {
   return (
     <BrowserRouter>
+     <Analytics />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/app/*" element={<Dashboard />} />
